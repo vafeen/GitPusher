d52b32f5e8e918285531696fae0d299d60a13dc6
Vafeen <vaf_art@mail.ru>
2024-02-22 12:05:05 +0300

    fix bug with losed string in main.py

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::time_t t = std::time(NULL);
     std::tm tm = *std::localtime(&t);
-    std::string message = "git commit -m \"" + to_string(tm.tm_mday) + '.' + to_string(tm.tm_mon + 1) + '.' + to_string(tm.tm_year + 1900) + " " + to_string(tm.tm_hour) + ':' + (tm.tm_min > 9 ? to_string(tm.tm_min) : '0' + to_string(tm.tm_min)) + "\"";
+    std::string message = "git commit -m \"" + to_string(tm.tm_mday) + '.' + (tm.tm_mon > 9 ? to_string(tm.tm_mon) : '0' + to_string(tm.tm_mon)) + '.' + to_string(tm.tm_year + 1900) + " " + to_string(tm.tm_hour) + ':' + (tm.tm_min > 9 ? to_string(tm.tm_min) : '0' + to_string(tm.tm_min)) + "\"";
     system("git add .");
     system(message.c_str());
     system("git push");
--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from datetime import datetime
 
 today = datetime.today()
 
-string = "i use git {}.{}.{} {}:{}".format(
-    today.day, today.month, today.year, today.hour, today.minute
+month = ""
+minute = ""
+if(today.month<10):
+    month = f"0{today.month}"
+else:
+    month = f"{today.month}"
+if(today.minute<10):
+    minute = f"0{today.minute}"
+else:
+    minute = f"{today.minute}"
+string = "{}.{}.{} {}:{}".format(
+    today.day, month, today.year, today.hour, minute
 )
 
 os.system("git add .")
-
+os.system(f"git commit -m \"{string}\"")
 os.system("git push")
